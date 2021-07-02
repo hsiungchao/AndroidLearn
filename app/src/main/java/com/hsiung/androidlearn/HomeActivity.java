67318bbf04cbf58126c24b079ce8638622914033
admin <adminzy@qq.com>
2021-07-02 15:25:05 +0800

    first commit

---
+++ app/src/main/java/com/hsiung/androidlearn/HomeActivity.java
@@ -0,0 +1,63 @@
+package com.hsiung.androidlearn;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBar;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.fragment.app.FragmentTransaction;
+
+import android.content.Context;
+import android.os.Bundle;
+import android.view.MenuItem;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+public class HomeActivity extends AppCompatActivity {
+
+    private Context mContext = this;
+    private UIFragment mUIFragment;
+    private EventFragment mEventFragment;
+    private ThirdPartyLibFragment mLibFragment;
+
+    private Toolbar mToolbar;
+    private BottomNavigationView mNavigationView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+
+        // 初始化Fragment
+        mUIFragment = UIFragment.newInstance(null, null);
+        mEventFragment = EventFragment.newInstance(null, null);
+        mLibFragment = ThirdPartyLibFragment.newInstance(null, null);
+
+        getSupportFragmentManager().beginTransaction().add(R.id.fl_container, mUIFragment).commitAllowingStateLoss();
+
+        // 隐藏默认的ActionBar
+        ActionBar actionBar = getSupportActionBar();
+        if (actionBar != null) {
+            actionBar.hide();
+        }
+
+        mToolbar = findViewById(R.id.toolbar);
+        mToolbar.setTitle(R.string.ui);
+        setSupportActionBar(mToolbar);
+
+        mNavigationView = findViewById(R.id.nav_bottom_navigation);
+        mNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                int id = item.getItemId();
+                if (id == R.id.nav_ui) {
+                    getSupportFragmentManager().beginTransaction().replace(R.id.fl_container, mUIFragment).commitAllowingStateLoss();
+                } else if (id == R.id.nav_event) {
+                    getSupportFragmentManager().beginTransaction().replace(R.id.fl_container, mEventFragment).commitAllowingStateLoss();
+                } else if (id == R.id.nav_third_party_lab) {
+                    getSupportFragmentManager().beginTransaction().replace(R.id.fl_container, mLibFragment).commitAllowingStateLoss();
+                }
+                return true;
+            }
+        });
+    }
+}
---
+++ app/src/main/java/com/hsiung/androidlearn/UIFragment.java
@@ -0,0 +1,89 @@
+package com.hsiung.androidlearn;
+
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+
+import com.hsiung.androidlearn.ui.TextViewActivity;
+
+public class UIFragment extends Fragment {
+
+    private static final String ARG_PARAM1 = "param1";
+    private static final String ARG_PARAM2 = "param2";
+
+    private String mParam1;
+    private String mParam2;
+    private Context mContext;
+
+    private Button mBtnTextView;
+
+    public UIFragment() {
+    }
+
+    public static UIFragment newInstance(String param1, String param2) {
+        UIFragment fragment = new UIFragment();
+        Bundle args = new Bundle();
+        args.putString(ARG_PARAM1, param1);
+        args.putString(ARG_PARAM2, param2);
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (getArguments() != null) {
+            mParam1 = getArguments().getString(ARG_PARAM1);
+            mParam2 = getArguments().getString(ARG_PARAM2);
+        }
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_ui, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        mBtnTextView = view.findViewById(R.id.btn_textView);
+
+        setListener();
+    }
+
+    @Override
+    public void onAttach(@NonNull Context context) {
+        super.onAttach(context);
+        mContext = context;
+    }
+
+    private void setListener() {
+        OnClick onClick = new OnClick();
+        mBtnTextView.setOnClickListener(onClick);
+    }
+
+    private class OnClick implements View.OnClickListener{
+        @Override
+        public void onClick(View v) {
+            int id = v.getId();
+            Intent intent = null;
+            if (id == R.id.btn_textView) {
+                // 跳转到TextView演示界面
+                intent = new Intent(mContext, TextViewActivity.class);
+            }
+            startActivity(intent);
+        }
+    }
+
+}
