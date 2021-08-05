a777e8e9e360231b51280a428be095d56766e5c7
admin <adminzy@qq.com>
2021-08-05 11:19:03 +0800

    增加ViewPager2学习内容 1、ViewPager2 2、ViewPager2 + Fragment 3、ViewPager2 + Fragment + TabLayout

--- app/src/main/java/com/hsiung/androidlearn/ui/tablayout/BlankFragment.java
+++ app/src/main/java/com/hsiung/androidlearn/ui/tablayout/BlankFragment.java
@@ -3,14 +3,19 @@ package com.hsiung.androidlearn.ui.tablayout;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.hsiung.androidlearn.R;
+import com.hsiung.androidlearn.ui.recyclerview.LinearAdapter;
 
 public class BlankFragment extends Fragment {
 
@@ -53,6 +58,19 @@ public class BlankFragment extends Fragment {
     private void initView() {
         TextView textView = mRootView.findViewById(R.id.tv_blank_fragment);
         textView.setText(mTextString);
+    }
 
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        RecyclerView recyclerView = view.findViewById(R.id.recycler_view);
+        recyclerView.setLayoutManager(new LinearLayoutManager(getActivity()));
+        recyclerView.setAdapter(new LinearAdapter(getActivity(), new LinearAdapter.OnItemClickListener() {
+            @Override
+            public void onClick(int position) {
+                Toast.makeText(getActivity(), "click..." + position, Toast.LENGTH_SHORT).show();
+            }
+        }));
     }
 }
